0ff74231bedb8fe6c08c6dbf385de983b4c29395
Nikita Glazkov <zitrnik@gmail.com>
2020-12-17 22:13:00 +0300

    DEV: add example serializer

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -17,10 +17,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path
+from django.urls.conf import include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1', include('boards.urls'))
 ]
 
 if settings.DEBUG:
