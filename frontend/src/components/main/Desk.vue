d5c65ee5880b4f4f4d79717caf60f1f7eaafad97
Nikita Glazkov <zitrnik@gmail.com>
2021-06-17 15:25:21 +0300

    DEV: update task card code

--- frontend/src/components/main/Desk.vue
+++ frontend/src/components/main/Desk.vue
@@ -11,48 +11,24 @@
       flat
     >
       <v-banner color="white" sticky>{{ status.title }}</v-banner>
-      <v-card
+      <Task
         v-for="(t, index) in 4"
         :key="t"
-        class="mb-4 h-100"
         :class="{ 'mt-4': index == 0 }"
-        shaped
-        outlined
-        elevation="2"
-      >
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title>Single-line item</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item two-line>
-          <v-list-item-content>
-            <v-list-item-title>Two-line item</v-list-item-title>
-            <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title>Three-line item</v-list-item-title>
-            <v-list-item-subtitle>
-              Secondary line text Lorem ipsum dolor sit amet,
-            </v-list-item-subtitle>
-            <v-list-item-subtitle>
-              consectetur adipiscing elit.
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-card>
+      ></Task>
     </v-card>
   </div>
 </template>
 
 <script>
+import Task from "@/components/main/Task.vue";
+
 export default {
   name: "Desk",
-  props: ["statusList"]
+  props: ["statusList"],
+  components: {
+    Task
+  }
 };
 </script>
 
---
+++ frontend/src/components/main/Task.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card class="mb-4 h-100 ml-2 mr-2" shaped outlined elevation="2">
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-container fluid>
+          <v-row>
+            <div class="text-overline">PJ1-213</div>
+            <v-spacer></v-spacer>
+            <v-chip class="" color="green" text-color="white">
+              <v-avatar left class="green darken-4"> 1 </v-avatar>
+              SP
+            </v-chip>
+          </v-row>
+        </v-container>
+        <v-list-item-subtitle>Спринт #1</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>Купить коту еды</v-list-item-title>
+        <!-- <v-list-item-subtitle>
+          Secondary line text Lorem ipsum dolor sit amet,
+        </v-list-item-subtitle> -->
+      </v-list-item-content>
+    </v-list-item>
+    <v-card-actions>
+      <v-btn rounded text>Подробнее</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn outlined rounded text>Button</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Task"
+};
+</script>
+
+<style lang="scss" scoped></style>
